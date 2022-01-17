4dd7effe247103d1e78b3c8abe9689eb43548b40
Ritik Kumar <kumarritik@microsoft.com>
2022-01-17 21:51:28 +0530

    Authentication System

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+// Import NPM Modules
 const express = require('express');
 const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
+const validator = require('validator');
 
+// Import Utils
 const { mongoURI } = require('./private-constants'); 
 const { cleanUpAndValidate } = require('./utils/AuthUtils');
+
+// Import Models
+const UserModel = require('./Models/UserModel');
 
 const app = express();
 
@@ -16,6 +22,7 @@ mongoose.connect(mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then((res) => {
+    // console.log(res);
     console.log('Connected to the database');
 })
 
@@ -37,8 +44,49 @@ app.get('/register', (req, res) => {
 })
 
 // Allow the user to login
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
 
+    const { loginId, password } = req.body;
+
+    let user;
+    if(validator.isEmail(loginId)) {
+        // loginId is a email
+        user = await UserModel.findOne({email: loginId});
+    }
+    else {
+        // loginId is a username
+        user = await UserModel.findOne({username: loginId});
+    }
+    
+    // User not found
+    if(!user) {
+        return res.send({
+            status: 401,
+            message: "User not found",
+            data: req.body
+        })
+    }
+
+    // Match the password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if(!isMatch) {
+        return res.send({
+            status: 401,
+            message: "Invalid Password",
+            data: req.body
+        })
+    }
+
+    return res.send({
+        status: 200,
+        message: "Login Successful",
+        data: {
+            name: user.name,
+            email: user.email,
+            username: user.username
+        }
+    })
 })
 
 // Allow the user to register
@@ -61,6 +109,30 @@ app.post('/register', async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 13);
 
     // Save the data to db
+
+    const user = new UserModel({
+        name,
+        email,
+        username,
+        password: hashedPassword
+    })
+
+    try {
+        const userDb = await user.save();
+
+        return res.send({
+            status: 200,
+            message: "Registration Successful",
+            data: userDb
+        })
+    }
+    catch(err) {
+        return res.send({
+            status: 400,
+            message: "Database error",
+            error: err
+        })
+    }
 })
 
 app.listen(PORT, () => {
@@ -70,3 +142,8 @@ app.listen(PORT, () => {
 // Create a folder
 // npm init
 // npm install express
+
+// ipconfig - Windows
+// ifconfig - linux
+
+// Lazy Loading
